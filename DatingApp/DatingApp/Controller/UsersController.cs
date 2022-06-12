1cd1c94f8f298ef88452b6bc433cc9247b285236
Rohit Ajmane <ajmane.rohit@outlook.com>
2022-06-12 11:53:43 +0530

    First Controller

---
+++ DatingApp/DatingApp/Controller/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
