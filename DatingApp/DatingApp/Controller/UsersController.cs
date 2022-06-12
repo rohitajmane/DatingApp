e2346a312021794e458cbf0cfd6994b3dff4d9aa
Rohit Ajmane <ajmane.rohit@outlook.com>
2022-06-12 12:02:47 +0530

    Convert API to Async Task Await

--- DatingApp/DatingApp/Controller/UsersController.cs
+++ DatingApp/DatingApp/Controller/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using DatingApp.API.Data;
 using DatingApp.API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controller
 {
@@ -16,15 +17,15 @@ namespace DatingApp.API.Controller
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return _context.Users.ToList();
+            return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
